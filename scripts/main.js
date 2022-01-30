44a3c86d9ebf0d8a8a373bdcde6e7995bb250fff
Phantom <phantom@phantom-games.com>
2022-01-30 00:32:05 -0800

    Add rowf badge shop and options to switch between vertical and horizontal trackers

--- scripts/main.js
+++ scripts/main.js
@@ -12,6 +12,18 @@ var currentKeyCounts = {
     4: 0,
 };
 
+var rowf_coins = 1650;
+
+const quizmoAnswers = [
+    3, 2, 2, 3, 3, 1, 3, 1, 3, 2,
+    1, 3, 2, 2, 1, 3, 1, 3, 2, 1,
+    1, 3, 2, 1, 3, 2, 2, 3, 3, 2, 
+    2, 2, 1, 3, 3, 3, 3, 1, 2, 2, 
+    3, 2, 3, 2, 3, 1, 1, 2, 2, 1, 
+    2, 3, 2, 1, 3, 3, 2, 3, 1, 3, 
+    3, 2, 2, 2
+]
+
 function localStorageGetWithDefault(key, defaultValue) {
     const value = localStorage.getItem(key);
     if (!value) {
@@ -28,6 +40,7 @@ $(document).ready(function(){
     $('img').contextmenu(function(){return false;});
 
     $('.optional-item').height(40);
+    $('.badge').height(40);
 
     // set text display for the main items
     $('.main-tracker img').on('mouseenter', function() {
@@ -38,25 +51,46 @@ $(document).ready(function(){
         $('.main-tracker h2').text("");
     });
 
-    // required chapter items
-    $("img[data-chapter]").click(function(){
-        var c = parseInt($(this).attr("data-chapter"));
-        if ($(this).hasClass("unselected")) {
-            $(this).removeClass("unselected");
-        } else {
-            $(this).addClass("unselected");
-        }
-
-        checkIfChapterIsCompletable(c);
+    $('.horizontal-tracker img').on('mouseenter', function() {
+        $('.horizontal-tracker h2').text($(this).attr('id'));
     });
 
-    // chapter keys
+    $('.horizontal-tracker img').on('mouseleave', function() {
+        $('.horizontal-tracker h2').text("");
+    });
+
+    // required chapter items
+    $(".badge").click(function(){
+        var cost = parseInt($(this).attr("data-badge-cost"));
+        if ($(this).hasClass("unselected")) {
+            $(this).removeClass("unselected");
+            rowf_coins -= cost;
+        } else {
+            $(this).addClass("unselected");
+            rowf_coins += cost;
+        }
+
+        $(".rowf-badges h2").text(rowf_coins);
+    });
+
     $("img[data-chapter-key]").click(function(){
         var c = parseInt($(this).attr("data-chapter-key"));
         $(this).removeClass("unselected");
         if (currentKeyCounts[c] < maxKeyCounts[c]) {
             ++currentKeyCounts[c];
-            $(`#chapter-${c}-key-count`).text(`${currentKeyCounts[c]}/${maxKeyCounts[c]}`);
+            $(`.chapter-${c}-key-count`).text(`${currentKeyCounts[c]}/${maxKeyCounts[c]}`);
+        }
+
+        if ($(this).attr("id") === "Quizmo") {
+            var nextQuizmo = currentKeyCounts[c] + 1;
+
+            if (nextQuizmo >= 65) {
+                $(".quizmo-answers h1").text(`Quizmo Is Done!`);
+                $(".quizmo-answers h2").text("");
+            } else {
+                $(".quizmo-answers h1").text(`Quizmo Answer (${nextQuizmo}):`);
+                $(".quizmo-answers h2").text(quizmoAnswers[currentKeyCounts[c] - 1]);
+            }
         }
     });
 
@@ -64,12 +98,42 @@ $(document).ready(function(){
         var c = parseInt($(this).attr("data-chapter-key"));
         if (currentKeyCounts[c] > 0) {
             --currentKeyCounts[c];
-            $(`#chapter-${c}-key-count`).text(`${currentKeyCounts[c]}/${maxKeyCounts[c]}`);
+            $(`.chapter-${c}-key-count`).text(`${currentKeyCounts[c]}/${maxKeyCounts[c]}`);
         }
 
-        if (currentKeyCounts[c] === 0) {
-            $(this).addClass("unselected");
+        if ($(this).attr("id") === "Quizmo") {
+            $(".quizmo-answers h1").text(`Quizmo Answer (${currentKeyCounts[c] + 1}):`);
+            $(".quizmo-answers h2").text(quizmoAnswers[currentKeyCounts[c] - 1]);
         }
         return false;
     });
+
+    // options menu
+    $(document).click(function(e) {
+        // if the option menu is open, and the click is outside the options menu, close it
+        var container = $("#options-menu");
+        if (container.hasClass("options-open") && !container.is(e.target) && container.has(e.target).length === 0) {
+            $("#options-menu-toggle").click();
+        }
+    });
+
+    $("#options-menu-toggle").click(function(e) {
+        e.stopPropagation();
+        $(this).toggleClass("options-open");
+        $("#options-menu").toggleClass("options-open");
+    });
+
+    $("#vertical-tracker").click(function() {
+        var isChecked = $(this).is(':checked');
+        $(".horizontal-tracker").toggle(isChecked);
+        $(".main-tracker").toggle(!isChecked);
+        localStorage.setItem("vertical-tracker", isChecked);
+    });
+
+    var vertical_tracker = localStorageGetWithDefault("vertical-tracker", false) == "true";
+    if (vertical_tracker) {
+        $("#vertical-tracker").click();
+    } else {
+        $(".horizontal-tracker").toggle();
+    }
 });
